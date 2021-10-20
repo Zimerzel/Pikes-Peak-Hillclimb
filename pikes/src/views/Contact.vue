7c78a6238690f479c9a3056c6b55c7be672c27c5
Imerzel <zach.imerzel@gmail.com>
2021-10-20 10:57:57 -0500

    added functional map back

--- pikes/src/views/Contact.vue
+++ pikes/src/views/Contact.vue
@@ -5,36 +5,63 @@
         <h1> Contact Us</h1>
         <img src="https://ppihc.org/wp-content/uploads/2015/09/inline-title.jpg" />
         <br/>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1358.651994457716!2d-104.8585542!3d38.7881659!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87135b5002769d2d%3A0x5fd1fbd815eeb2f1!2sPikes+Peak+International+Hill+Climb!5e1!3m2!1sen!2sus!4v1513195082810" width="800" height="550" frameborder="0" style="border: 0px; pointer-events: none;" allowfullscreen=""></iframe>
+        <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLZ12AlBbE4cR8bLuFdj70V8&key=AIzaSyCnzWZuKfAzAXehmGcjwBQcciP8slDHFQM"></iframe>
         <br/>
-        <div class="contact-info">
-            <h2>Office Adress</h2>
-            <p>1631 Mesa Avenue, Suite E</p>
-            <br />
-            <p>Colorado Springs, CO 80906</p>
-            <br />
-            <h2>Phone Number</h2>
-            <p>Main: 719.685.4400</p>
-            <br />
-            <h2>General Information</h2>
-            <p>Information@pphic.org</p>
-            <br />
-            <h2>Staff Email</h2>
-            <p> zach.imerzel@gmail.com</p>
+        <div id="contact-container">
+            <div class="contact-info">
+                <img src="" />
+                <h2>Office Adress</h2>
+                <p>1631 Mesa Avenue, Suite E</p>
+                <br />
+                <p>Colorado Springs, CO 80906</p>
+                <br />
+                <img src="" />
+                <h2>Phone Number</h2>
+                <p>Main: 719.685.4400</p>
+                <br />
+                <img src="" />
+                <h2>General Information</h2>
+                <p>Information@pphic.org</p>
+                <br />
+                <img src="" />
+                <h2>Staff Email</h2>
+                <p> zach.imerzel@gmail.com</p>
+            </div>
         </div>
     </div>
 </template>
 <style scoped>
+iframe{
+    width: 85%;
+}
+
 .contact {
     padding-top: 15vh;
+    background-color: rgb(255, 255, 255);
 }
 
-.contact h2{
+.contact h1{
+    color: rgb(0, 0, 0);
+}
+
+#contact-container{
+    padding-right:7vw;
+    padding-left: 7vw;
+}
+
+.contact-info{
+background-color: #2c3e50;
+padding: 20px;
+}
+
+.contact-info h2{
     padding: 10px;
+    color: white;
 }
 
-.contact p{
+.contact-info p{
     display: inline;
     padding: 10px;
+    color: white;
 }
 </style>
