5dc46d9dac98ba60b6a09c3fd6736ef6d43b61e9
Imerzel <zach.imerzel@gmail.com>
2021-11-15 13:24:08 -0600

    inital service worker

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
